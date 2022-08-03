124e3619d2aae8852d64c3de0d652e05cbe5bd34
nur-j <jumayev.nur@gmail.com>
2022-08-03 13:09:43 +0500

    admin lte admin image deleted and renamed

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -165,10 +165,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{ asset('assets/admin/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+          {{-- <img src="{{ asset('assets/admin/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image"> --}}
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Admin</a>
         </div>
       </div>
 
